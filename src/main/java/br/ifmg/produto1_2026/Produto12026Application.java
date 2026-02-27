e664aac26550a4d3e1ed1f51d04c8b45035ff69f
Thiago Nery <thiagojoseneryoliveira@gmail.com>
2026-02-27 10:48:40 -0300

    01 - criacao do projeto

---
+++ src/main/java/br/ifmg/produto1_2026/Produto12026Application.java
@@ -0,0 +1,13 @@
+package br.ifmg.produto1_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Produto12026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Produto12026Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/ifmg/produto1_2026/Produto12026ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifmg.produto1_2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Produto12026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
